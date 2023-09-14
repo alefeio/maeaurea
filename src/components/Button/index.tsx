d839634237e1fc4c0be103787fa393d4e357cf96
Alexandre Feio <alexandre.feio@bussoladoinvestidor.com>
2023-09-14 04:20:44 -0300

    new

--- src/components/Button/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./styles.module.scss";
-
-export function Button(props) {
-  return (
-    <a href={props.link} className={styles.subscribeButton}>
-      {props.children}
-    </a>
-  );
-}
